e96c643e2466bfc72b59a58a245385939b568c39
tmusenko <66254894+tmusenko@users.noreply.github.com>
2021-01-25 01:47:14 +0000

    Update photo.php

--- php/photo.php
+++ php/photo.php
@@ -1,29 +1,29 @@
+  
 <?php
 
-	$executionStartTime = microtime(true) / 1000;
+    ini_set('display_errors', 'On');
+    error_reporting(E_ALL);
 
-	$url='https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=' . $_REQUEST['q'] . '';
-	
+    // Open Exchange Rates
 
-	$ch = curl_init();
+    $openExchangeRatesUrl = "https://openexchangerates.org/api/latest.json?app_id=4689172d1bf54c9da7fa08c0fcae130d";
+
+    $ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_URL,$openExchangeRatesUrl);
+    
+	$openExchangeRatesResult=curl_exec($ch);
+    curl_close($ch);
+    $openExchangeRatesResult = json_decode($openExchangeRatesResult,true);	
 
-	$result=curl_exec($ch);
-
-	curl_close($ch);
-	$decode = json_decode($result,true);
-	
-	
+    // Final Output
 	$output['status']['code'] = "200";
-	$output['status']['name'] = "ok";
-	$output['status']['description'] = "mission saved";
-	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-	$output['data'] = $decode;
+	$output['status']['name'] = "OK";
+    $output['openExchangeRates'] = $openExchangeRatesResult;
 
-	
 	header('Content-Type: application/json; charset=UTF-8');
+
 	echo json_encode($output);
 
 ?>
