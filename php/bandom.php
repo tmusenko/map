ade3f8092095a4e851befa440a7e9d4f60557a57
tmusenko <66254894+tmusenko@users.noreply.github.com>
2021-03-04 22:29:34 +0000

    Update bandom.php

--- php/bandom.php
+++ php/bandom.php
@@ -1,31 +1,5 @@
 <?php
 
-//     $executionStartTime = microtime(true);
-
-//     $countryData = json_decode(file_get_contents("../json/borders.json"), true);
-
-// $ch = curl_init();
-// 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// 	curl_setopt($ch, CURLOPT_URL,$countryData);
-
-// 	$result=curl_exec($ch);
-
-// 	curl_close($ch);
-
-// 	$decode = json_decode($result,true);	
-
-// 	$output['status']['code'] = "200";
-// 	$output['status']['name'] = "ok";
-// 	$output['status']['description'] = "mission saved";
-// 	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-// 	$output['data'] = $decode;
-	
-// 	header('Content-Type: application/json; charset=UTF-8');
-
-// 	echo json_encode($output); 
-
-
 
     $executionStartTime = microtime(true);
 
@@ -36,20 +10,18 @@
     foreach ($countryData['features'] as $feature) {
         $temp = null;
         $temp = $feature;
-        // $temp['name'] = $feature['properties']['name'];
+       
 
         array_push($country, $temp);
     }
 
-    // usort($country, function ($item1, $item2) { 
-    //     return $item1['name'] <=> $item2['name'];
-    // });
+
 
     $output['status']['code'] = "200";
     $output['status']['name'] = "ok";
     $output['status']['description'] = "success";
     $output['status']['executedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-    // $output['data'] = $decode;
+
     
     $output['data'] = $country;
  
